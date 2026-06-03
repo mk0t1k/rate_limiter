9d1a21438d15b74a52f9a8f78c44a2de274c2ce0
justNotherbot <91804283+justNotherbot@users.noreply.github.com>
2026-06-03 12:15:50 +0300

    Interface attempt

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
-int main() {
-  return 0;
-}
+template<class T>
+struct SlidingWindowLog {
+	void SetCapacity(size_t tgt);
+	
+	void SetTimeWidth(double cnt_sec);
+};
+
+template<class T>
+struct TokenBucket {
+	void SetCapacity(size_t tgt);
+	
+	void SetRefillRate(double tk_per_sec);
+};
+
+template<tepmlate<typename> typename Feature>
+struct IRateLimiter : Feature<IRateLimiter> {
+	bool SendRequest(const key_type& key); // What is key_type ?
+};
