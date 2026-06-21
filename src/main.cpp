566ff492e31c4bfd5895e65ad8d35ec1bc33ab6f
justNotherbot <tcookermann@gmail.com>
2026-06-21 16:05:42 +0300

    Update demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,17 @@
 #include "lib/mutex_storage.hpp"
 #include "lib/sharded_storage.hpp"
 #include "lib/meta.hpp"
+#include "lib/ttl.hpp"
 
+#include <cassert>
 #include <iostream>
 #include <vector>
 
 int main() {
-  avito_limiter::SlidingWindowAlgo limiter{3U, 4.0F};
+  avito_limiter::SlidingWindowAlgo limiter{3U, 4.0F, 3.0};
   std::vector<avito_limiter::key_type> keys = {"ab", "cd", "ef"};
-  avito_limiter::IRateLimiter* intfc = new avito_limiter::ShardedWinLimiter<3U, true>{
+  avito_limiter::IRateLimiter* intfc = new avito_limiter::MutexWinLimiter{
     keys.begin(), keys.end(), limiter};
-  //avito_limiter::ShardedStorage<3, 
-  //  avito_limiter::key_type, avito_limiter::SlidingWindowAlgo> s_sharded{
-  //  keys.begin(), keys.end(), limiter};
-  
   while (true) {
     std::string key_name;
     std::cin >> key_name;
