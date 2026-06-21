1f4beab49f25649593a9e2e56d3234c7913db786
Nikita <nikitakokonov03@gmail.com>
2026-06-21 18:20:31 +0300

    add Clock as a template parameter for algorithms

--- lib/sliding_win_log.hpp
+++ lib/sliding_win_log.hpp
@@ -11,8 +11,14 @@
 
 namespace avito_limiter {
 
-class SlidingWindowAlgo final : public AlgBase<SlidingWindowAlgo> {
-  using clock_type = std::chrono::steady_clock;
+template <typename Clock = std::chrono::steady_clock>
+class SlidingWindowAlgo final : public AlgBase<SlidingWindowAlgo<Clock>, Clock> {
+public:
+  using clock_type = Clock;
+private:
+  using AlgBase_t = AlgBase<SlidingWindowAlgo<Clock>, Clock>;
+  using TtlValue_t = TtlValue<Clock>;
+
   using time_point = decltype(clock_type::now());
   using duration_type = typename clock_type::duration;
 
@@ -22,17 +28,36 @@ class SlidingWindowAlgo final : public AlgBase<SlidingWindowAlgo> {
   std::size_t capacity_ = 0U;
   float time_offs_sec_ = 0.0F;
 
-  void Update() const;
+  void Update() const {
+    time_point curr = clock_type::now();
+    std::chrono::duration<float> offset_duration(time_offs_sec_);
+    duration_type casted_offset = 
+      std::chrono::duration_cast<duration_type>(offset_duration);
+    time_point cull_point = curr - casted_offset;
+    while (accesses_.size() && accesses_.top() < cull_point) {
+      accesses_.pop();
+    }
+  }
 public:
-  using time_val_t = typename AlgBase::value_type;
+  using time_val_t = typename AlgBase_t::value_type;
 
   SlidingWindowAlgo() = default;
 
-  SlidingWindowAlgo(
-    std::size_t capacity, float dur_sec, time_val_t ttl_val=TtlValue::kNoTtl);
+  SlidingWindowAlgo(std::size_t capacity, float dur_sec, time_val_t ttl_val = TtlValue_t::kNoTtl) : 
+    AlgBase_t{ttl_val}, capacity_{capacity}, time_offs_sec_{dur_sec} {}
 
-  bool Verify();
+  bool Verify() {
+    Update();
+    if(accesses_.size() == capacity_) {
+      return false;
+    }
+    accesses_.push(clock_type::now());
+    return true;
+  }
 
-  std::size_t GetAvail() const noexcept;
+  std::size_t GetAvail() const noexcept {
+    Update();
+    return static_cast<std::size_t>(capacity_ - accesses_.size());
+  }
 };
 } // namespace avito_limiter
--- lib/token_bucket.hpp
+++ lib/token_bucket.hpp
@@ -10,8 +10,19 @@
 
 namespace avito_limiter {
 
-class TokenBucketAlgo final: public AlgBase<TokenBucketAlgo> {
-  using clock_type = std::chrono::steady_clock;
+namespace {
+  constexpr float kTokBuckEpsilon = 1e-4;
+  constexpr float kTokBuckUnitPos = 1.0F - kTokBuckEpsilon;
+} // namespace
+
+template <typename Clock = std::chrono::steady_clock>
+class TokenBucketAlgo final: public AlgBase<TokenBucketAlgo<Clock>, Clock> {
+public:
+  using clock_type = Clock;
+private:
+  using TtlValue_t = TtlValue<Clock>; 
+  using AlgBase_t = AlgBase<TokenBucketAlgo<Clock>, Clock>;
+
   using time_point = decltype(clock_type::now());
 
   mutable time_point last_access_;
@@ -19,17 +30,40 @@ class TokenBucketAlgo final: public AlgBase<TokenBucketAlgo> {
   mutable float curr_tok_ = 0.0F;
   float capacity_ = 0.0F;
 
-  void Update() const noexcept;
+  void Update() const noexcept {
+    time_point curr = clock_type::now();
+    float dur_sec = std::chrono::duration<float>(curr - last_access_).count();
+    last_access_ = curr;
+    float cnt_refill = std::min(capacity_, refill_tok_sec_ * dur_sec);
+    curr_tok_ = std::min(capacity_, cnt_refill + curr_tok_);
+  }
+
 public:
-  using time_val_t = typename AlgBase::value_type;
+  using time_val_t = typename AlgBase_t::value_type;
 
-  TokenBucketAlgo();
+  TokenBucketAlgo() : last_access_{clock_type::now()} {}
   
-  TokenBucketAlgo(float v_refill, std::size_t capacity, 
-    time_val_t ttl_val=TtlValue::kNoTtl);
+  TokenBucketAlgo(float v_refill, std::size_t capacity, time_val_t ttl_val=TtlValue_t::kNoTtl) : 
+    AlgBase_t{ttl_val},
+    last_access_{clock_type::now()}, 
+    refill_tok_sec_{v_refill} 
+    {
+      capacity_ = static_cast<float>(capacity);
+      curr_tok_ = capacity_;
+    }
 
-  bool Verify() noexcept;
+  bool Verify() noexcept {
+    Update();
+    if(curr_tok_ >= kTokBuckUnitPos) {
+      curr_tok_ -= 1.0F;
+      return true;
+    }
+    return false;
+  }
 
-  std::size_t GetAvail() const noexcept;
+  std::size_t GetAvail() const noexcept {
+    Update();
+    return static_cast<std::size_t>(curr_tok_);
+  }
 };
 } // namespace avito_limiter
