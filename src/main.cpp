5c96ba59e60d97aa4fcb0cb689b59e56cef6a5e9
justNotherbot <tcookermann@gmail.com>
2026-06-22 19:10:24 +0300

    Update demo

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,24 @@
 #include "lib/meta.hpp"
 #include "lib/ttl.hpp"
 #include "lib/sliding_win_log.hpp"
+#include "lib/leaky_bucket.hpp"
 
 #include <cassert>
 #include <iostream>
 #include <vector>
 
+
 int main() {
-  avito_limiter::StoredData<avito_limiter::SlidingWindowAlgo> limiter{std::nullopt, 3U, 4.0F};
-  std::vector<avito_limiter::key_type> keys = {"ab", "cd", "ef"};
+  std::vector<avito_limiter::key_type> keys = {"a", "cd", "ef"};
   //avito_limiter::MutexStorage<avito_limiter::TokenBucketAlgo, 
   //  avito_limiter::key_type> stor{keys.begin(), keys.end(), 
   //    std::tuple<std::size_t, float>{3Z, 4.0F}, std::nullopt};
-  avito_limiter::IRateLimiter* intfc = new avito_limiter::MutexWinLimiter{
-    keys.begin(), keys.end(), std::tuple<std::size_t, float>{3Z, 4.0F}
-    , 4.0, 1.0};
+  avito_limiter::IRateShaper* shaper = new avito_limiter::LeakyBucketShaper{
+    3, 2, 1.0
+  };
+  
+  avito_limiter::IRateLimiter* intfc = new avito_limiter::MutexTokenLimiter{
+    keys.begin(), keys.end(), std::tuple<std::size_t, float>{3Z, 2.0F}};
   while (true) {
     std::string key_name;
     std::cin >> key_name;
@@ -25,7 +29,9 @@ int main() {
       break;
     }
     std::cout << intfc->Exists(key_name) << " " << intfc->Access(key_name) 
-      << " " << intfc->GetNumAvail(key_name) << "\n";
+      << " " << intfc->GetNumAvail(key_name) << " " << 
+      static_cast<bool>(shaper->AddRequest(key_name)) << " " << shaper->GetNumAvail() << "\n";
   }
   delete intfc;
+  delete shaper;
 }
